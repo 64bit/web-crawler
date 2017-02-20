7f6e14a3c07df4476d0ca2499a54f6d2be292667
Himanshu Neema <himanshun.iitkgp@gmail.com>
2017-02-20 13:36:08 -0500

    more unit tests

--- test/test_webpage.py
+++ test/test_webpage.py
@@ -11,8 +11,12 @@ class TestWebPage(unittest.TestCase):
   NO_LINKS_HTML = "<html> <body>Hello World</body> </html>"
   JS_LINKS_HTML = "<html> <head> <script src='/source.js'></script></head> <body>Hello World <script src='/source2.js'></script></body> </html>"
   STYLESHEET_LINKS_HTML = "<html> <head> <link href='/source.css' rel='stylesheet'/> </head> <body>Hello Word</body> </html>"
+  IMAGE_LINKS_HTML = "<html> <body><img src='/funny.gif' /> Hello Word</body> </html>"
+  ANCHOR_LINKS_HTML = "<html> <body> Hello Word <a href='//what.html' /> <a href='/fetch.html' /> </body> </html>"
+  FILE_LINKS_HTML = "<html> <body> Hello Word <a href='/source.pdf' /> <a href='source.txt' /> </body> </html>"
   
   def start_server(self, html):
+    # Tests could mock the requests.get(url) call but lets run a real server for testing
     def run_app(args):
       app = Flask("testWebPage")
       @app.route("/")
@@ -57,4 +61,34 @@ class TestWebPage(unittest.TestCase):
     self.assertEqual(0, len(webpage.get_anchors()))
     self.assertEqual(0, len(webpage.get_images()))
     self.assertEqual(0, len(webpage.get_files()))
+
+  def test_image_links(self):
+    self.start_server(TestWebPage.IMAGE_LINKS_HTML)
+    webpage = WebPage(TestWebPage.SERVER) 
+    self.assertEqual(0, len(webpage.get_js())) 
+    self.assertEqual(0, len(webpage.get_stylesheets()))
+    self.assertEqual(0, len(webpage.get_links()))
+    self.assertEqual(0, len(webpage.get_anchors()))
+    self.assertEqual(1, len(webpage.get_images()))
+    self.assertEqual(0, len(webpage.get_files()))
+
+  def test_anchor_links(self):
+    self.start_server(TestWebPage.ANCHOR_LINKS_HTML)
+    webpage = WebPage(TestWebPage.SERVER) 
+    self.assertEqual(0, len(webpage.get_js())) 
+    self.assertEqual(0, len(webpage.get_stylesheets()))
+    self.assertEqual(0, len(webpage.get_links()))
+    self.assertEqual(2, len(webpage.get_anchors()))
+    self.assertEqual(0, len(webpage.get_images()))
+    self.assertEqual(0, len(webpage.get_files()))
+    
+  def test_file_links(self):
+    self.start_server(TestWebPage.FILE_LINKS_HTML)
+    webpage = WebPage(TestWebPage.SERVER) 
+    self.assertEqual(0, len(webpage.get_js())) 
+    self.assertEqual(0, len(webpage.get_stylesheets()))
+    self.assertEqual(0, len(webpage.get_links()))
+    self.assertEqual(2, len(webpage.get_anchors()))
+    self.assertEqual(0, len(webpage.get_images()))
+    self.assertEqual(2, len(webpage.get_files()))
     
