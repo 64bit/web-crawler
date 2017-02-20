08c7d14aaea39392dda2e184f8b20786d5b61d25
Himanshu Neema <himanshun.iitkgp@gmail.com>
2017-02-20 00:18:10 -0500

    stylesheets & files for assets; README

--- webpage.py
+++ webpage.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup as BS
 import requests
 from urlparse import urlparse
 from urlparse import urljoin
+import mimetypes
+
+mimetypes.init()
 
 class WebPage(object):
 
@@ -15,10 +18,11 @@ class WebPage(object):
     assets = []
     assets += self.get_js()
     assets += self.get_images()
-    assets += self.get_links()
+    assets += self.get_stylesheets()
+    assets += self.get_files()
     return map(lambda a: a.geturl(), assets) 
 
-  def __resource(self, element, attribute, keep_fragments=True):
+  def __get_url(self, element, attribute, keep_fragments=True):
     resources = []
     resources += self.soup.find_all(element)
     url_strings = filter(lambda val: val is not None, map(lambda res: res.get(attribute), resources))
@@ -26,7 +30,6 @@ class WebPage(object):
     # parse url as well as convert relative url to absolute url
     def parse_url(url_str):
       # TODO handle tel:11234567890, mailto:someone@somewhere.com
-      # TODO handle fragments?
       absolute_url = urljoin(self.page.url, url_str) 
 
       if not keep_fragments:
@@ -41,13 +44,40 @@ class WebPage(object):
     return urls
 
   def get_js(self):
-    return self.__resource('script', 'src')
+    return self.__get_url('script', 'src')
 
   def get_images(self):
-    return self.__resource('img', 'src')
+    return self.__get_url('img', 'src')
 
   def get_links(self):
-    return self.__resource('link', 'href')
+    return self.__get_url('link', 'href')
 
   def get_anchors(self, keep_fragments=True):
-    return self.__resource('a', 'href', keep_fragments)
+    return self.__get_url('a', 'href', keep_fragments)
+
+  def get_stylesheets(self):
+    stylesheets = []
+    stylesheets += self.soup.find_all('link')
+    def sytlesheet_filter(link):
+      rel = link.get('rel')[0]      
+      if rel == "stylesheet" or rel == "" or rel is None:
+        return True
+      return False    
+
+    stylesheets = filter(sytlesheet_filter, stylesheets)
+    url_strings = map(lambda ss: ss.get('href'), stylesheets)
+    urls = map(lambda ss: urlparse(urljoin(self.page.url, ss)), url_strings)
+    return urls
+
+  def get_files(self):
+    all_anchors = self.get_anchors()
+    
+    def filter_files(f):
+      if "." + f.geturl().split(".")[-1] in mimetypes.types_map.keys(): 
+        return True
+      return False
+  
+    return filter(filter_files, all_anchors)
+
+
+  
